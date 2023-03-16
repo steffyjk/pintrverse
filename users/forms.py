262c5c9e76bd61c22480b0e74b9b9285471a19a5
steffyjk <steffy.jk2018@gmail.com>
2023-03-16 23:24:35 +0530

    initial commit [ project upload ] - added user module [ registration & login , logout feat. ]

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from users.models import User
+from django.core.exceptions import ValidationError
+
+
+class UserRegistrationForm(forms.ModelForm):
+    confirm_password = forms.CharField(max_length=128)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name',
+                  'email', 'mobile_number', 'password', 'confirm_password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise ValidationError('Confirm password & Password must be same.')
+
