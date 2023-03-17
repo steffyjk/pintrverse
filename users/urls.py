770ac9739ba4b570b91077a5310b9ad5631663ac
steffyjk <steffy.jk2018@gmail.com>
2023-03-17 14:16:17 +0530

    Feat[User-profile] user profile update feature added

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from users.views import RegistrationView, HomeView
+from users.views import RegistrationView, HomeView, UserProfileUpdateView
 
+app_name = 'users'
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('home/', HomeView.as_view(), name='home'),
@@ -10,5 +11,6 @@ urlpatterns = [
         template_name='users/login.html',
         redirect_authenticated_user=True),
          name='login'),
-    path('logout/', LogoutView.as_view(next_page='/users/login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/users/home/'), name='logout'),
+    path('profile/edit/<int:pk>/', UserProfileUpdateView.as_view(), name='profile_edit'),
 ]
