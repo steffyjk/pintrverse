99cb95944aba72398147623148080a9acbf6413b
steffyjk <steffy.jk2018@gmail.com>
2023-04-04 17:57:44 +0530

    Feat:removed fetch history and all function - trying to deploy it on AWS add admin models for admin panel

--- pintrverse_app/admin.py
+++ pintrverse_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from pintrverse_app.models import Category, Tag
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Tag)
+class TagModelAdmin(admin.ModelAdmin):
+    pass
