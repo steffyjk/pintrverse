43684982404bb3dbd22fed045bc4094523905329
steffyjk <steffy.jk2018@gmail.com>
2023-04-06 01:45:58 +0530

    Feat:[Update]: added email configuration in settings.py for reset pwd functionality

--- pintrverse/settings.py
+++ pintrverse/settings.py
@@ -90,6 +90,14 @@ WSGI_APPLICATION = 'pintrverse.wsgi.application'
 # Database [ CUSTOM POSTGRES ]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
