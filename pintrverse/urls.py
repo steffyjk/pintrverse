621e30021c5a04676e5c95615f8e4ceff5f2a77b
steffyjk <steffy.jk2018@gmail.com>
2023-04-05 11:54:02 +0530

    Feat:removed fetch history and all function - trying to deploy it on AWS change home page

--- pintrverse/urls.py
+++ pintrverse/urls.py
@@ -18,11 +18,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from pintrverse_app.views import ListAllPins
 from users.views import HomeView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', HomeView.as_view(), name='home'),
+                  # path('', HomeView.as_view(), name='home'),
+                  path('', ListAllPins.as_view(), name='home'),
                   path('users/', include('users.urls', namespace='users'), ),
                   path('pintrverse/', include('pintrverse_app.urls', namespace='pintrverse'), ),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
